ffe31473b66f6efb4b17c2313cd525c842e9b8e1
titan0697 <titan_barquero@hotmail.com>
2023-06-02 14:33:42 -0600

    assert

---
+++ asserts.dart
@@ -0,0 +1,38 @@
+void main(){
+
+  final mySquare = Square ( side: 10);
+  
+  mySquare.side = 5;
+  
+  print('area: ${mySquare.area}');
+  
+}
+
+
+class Square {
+  
+  
+ double _side; //side * side //propiedad privada
+  
+  
+  Square({ required double side})
+    : assert(side >= 0, 'side must be >=0'),
+      _side = side;
+  
+  double get area {
+    return _side * _side;
+  }
+  
+  set side(double value){
+  print('setting new value $value');
+    if(value < 0) throw 'value must be >0';
+    
+    _side = value;
+  }
+  
+  double calculateArea(){
+    
+    return _side * _side;
+  }
+  
+}
