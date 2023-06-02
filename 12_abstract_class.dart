8825d006d155736ac2fb30c9626329d16cf2e186
titan0697 <titan_barquero@hotmail.com>
2023-06-02 14:56:30 -0600

    clases abstractas

---
+++ 12_abstract_class.dart
@@ -0,0 +1,48 @@
+void main(){
+
+  final windPlant = WindPlant(initialEnergy: 100);
+  
+  print(windPlant);
+  
+  print('wind: ${chargePhone(windPlant)}');
+}
+
+
+double chargePhone(EnergyPlant plant ){
+  if(plant.energyLeft < 10){
+  throw Exception('Not Enough energy');
+  }
+  
+  return plant.energyLeft - 10;
+}
+
+
+
+
+enum PlantType { nuclear, wind, water}
+
+abstract class EnergyPlant {
+
+  double energyLeft;
+  PlantType type; //nuclear, wind, water
+  
+  EnergyPlant({ 
+    required this.energyLeft,
+    required this.type
+    });
+  
+  void consumeEnergy( double amount );
+
+}
+
+//extends o implement
+class WindPlant extends EnergyPlant{
+
+WindPlant({required double initialEnergy})
+          : super(energyLeft: initialEnergy, type:PlantType.wind);
+  
+  @override
+  void consumeEnergy( double amount){
+  energyLeft -= amount;
+  }
+}
